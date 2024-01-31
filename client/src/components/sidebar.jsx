e54c42aa20ef636f63a6d65aeb54c9383050857a
Samux33 <samuelagustin332008@gmail.com>
2024-01-31 20:31:39 -0300

    componente SideBar

--- client/src/components/sidebar.jsx
+++ client/src/components/sidebar.jsx
@@ -1,7 +1,26 @@
-const SideBar=()=>{
-    return (
-        <>
-        
-        </>
-    )
-}
+import DHlogo from '../assets/logo-dh.png'
+
+const SideBar = () => {
+  return (
+    <aside>
+      <img src={DHlogo} alt="logo de digital house"/>
+      <h2>Digital House</h2>
+      <nav>
+        OPCIONES
+        <ul>
+          <li>
+            <a href=""><i className="fa-solid fa-building"></i> - Empresas</a>
+          </li>
+          <li>
+            <a href=""><i className="fa-solid fa-user"></i> - Aspirantes</a>
+          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default SideBar
